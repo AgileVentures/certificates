651b164db230aba2af04f002d6ce309e9b3cee37
Thomas Ochman <thomas@nocebo.se>
2014-04-23 23:03:10 +0200

    clean up. Added data.csv

--- cert_generator.rb
+++ cert_generator.rb
@@ -10,35 +10,34 @@ $:.unshift File.dirname(__FILE__)
 gem 'prawn'
 gem 'titleize'
 
+
 require 'rubygems'
 require 'prawn'
-require 'ostruct'
 require 'titleize'
-
+require 'date'
 
 
 @username = 'thomas ochman'
 @bg_image = File.join(File.dirname(__FILE__), "pdf/AV102-certificate300.jpg")
-@output = "pdf/full_template.pdf"
+#@output = "pdf/full_template.pdf"
 
 
 
-def write_to_cert(options = {}) 
-  defaults = {:name => @username, :date => '2014-01-01'}
+def write_to_cert(options = {})
+  defaults = {:name => @username, :date => Date.today.to_s}
   options = defaults.merge(options)
   name = options.fetch(:name)
-  date = options.fetch(:date)
-  
-    File.delete(@output) if File.exist?(@output)
-    Prawn::Document.generate(@output, :page_size => "A4", :background => @bg_image, :background_scale => 0.2432, :page_layout => :landscape, :left_margin => 30, :right_margin => 40, :top_margin =>7, :skip_encoding => true, ) do |pdf|
-      #pdf.start_new_page
-      #pdf.image @bg_image, :scale => 0.2375, :position => :center
-      #pdf.image @bg_image, :scale => 0.2300, :position => :center, :vposition => :center
+  date = Date.parse(options.fetch(:date))
+
+ 
+  output = "pdf/#{name}-#{date}.pdf"
+    File.delete(output) if File.exist?(output)
+    Prawn::Document.generate("pdf/#{name}-#{date}.pdf", :page_size => "A4",:background => @bg_image, :background_scale => 0.2431, :page_layout => :landscape, :left_margin => 30, :right_margin => 40, :top_margin => 7, :bottom_margin => 0, :skip_encoding => true, ) do |pdf|
       pdf.move_down 230
       pdf.font "Helvetica"
-      pdf.text name.titleize, :size => 48, :style => :bold, :color => "F07F48", :indent_paragraphs => 7
+      pdf.text name.titleize, :size => 48, :style => :bold, :color => "F07F48", :indent_paragraphs => 10
       pdf.move_up 165
-      pdf.text "Issued at #{date}", :size => 14, :style => :normal, :color => "575756", align: :right
+      pdf.text date.strftime("Issued on %A, %B %e, %Y"), :size => 14, :style => :normal, :color => "575756", align: :right
 
     end
 end
