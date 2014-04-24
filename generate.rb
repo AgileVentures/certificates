0c4269bdac8ba9344a8497cace19244f697916cc
Thomas Ochman <thomas@nocebo.se>
2014-04-24 11:56:30 +0200

    updated the data file to edx structure

--- generate.rb
+++ generate.rb
@@ -14,14 +14,16 @@ def generate
   csv = CSV.parse(csv_text, :headers => true)
   csv.each do |row|
     students = row.to_hash
-    email = students['Email']
-    write_to_cert(name: (students['Name']))
-    puts 'Generated certificate for:'.bold + students['Name'].bold.green
-    if email.nil?
-      puts 'No email for '.bold.red + students['Name'].bold.green + ' present!'.bold.red
-    else
-      send_mail((students['Name']), email, @output)
-      puts 'Generated email to:'.bold + students['Name'].bold.green
+    email = students['email']
+    if (students['grade'].to_i) >= 0.89
+      write_to_cert(name: (students['name'])) 
+      puts 'Generated certificate for:'.bold + students['name'].bold.green
+      if email.nil?
+        puts 'No email for '.bold.red + students['name'].bold.green + ' present!'.bold.red
+      else
+        send_mail((students['name']), email, @output)
+        puts 'Generated email to:'.bold + students['name'].bold.green
+      end
     end
   end
 end
