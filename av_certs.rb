81178af5788b2b1457c54d1c4c4480c0fba5a1f5
Thomas Ochman <thomas@nocebo.se>
2014-04-24 10:15:38 +0200

    added content type to mailer and updated the contitions for sending email

--- av_certs.rb
+++ av_certs.rb
@@ -45,12 +45,13 @@ def send_mail(name, email, file)
     to       "#{name} <#{email}>"
     subject  'AV-102 Certificate'
     body     File.read('data/body.txt')
-    add_file :filename => file, :content => File.read(file)
+    add_file :filename => file, :mime_type => 'application/x-pdf', :content => File.read(file)
   end
-
+  #mail.attachments[file] = { :mime_type => 'application/x-pdf', :content => File.read(file) }
   #mail.delivery_method :sendmail
-
+  mail.delivery_method :smtp, address: 'send.one.com', port: 1025
   #mail.deliver
+  puts mail
 end
 
 
--- generate.rb
+++ generate.rb
@@ -14,12 +14,13 @@ def generate
   csv = CSV.parse(csv_text, :headers => true)
   csv.each do |row|
     students = row.to_hash
+    email = students['Email']
     write_to_cert(name: (students['Name']))
     puts 'Generated certificate for:'.bold + students['Name'].bold.green
-    unless students['Email'].present?
+    if email.nil?
       puts 'No email for '.bold.red + students['Name'].bold.green + ' present!'.bold.red
     else
-      send_mail((students['Name']), (students['Email']), @output)
+      send_mail((students['Name']), email, @output)
       puts 'Generated email to:'.bold + students['Name'].bold.green
     end
   end
