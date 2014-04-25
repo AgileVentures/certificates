271bb56b7a78c3296180e86a97e81f54f6f9c443
Thomas Ochman <thomas@nocebo.se>
2014-04-25 12:23:43 +0200

    update in av_cert_spec.rb

---
+++ spec/av_cert_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../av_certs.rb' 
+require 'spec_helper.rb' 
+
+describe 'write_to_cert' do	
+	it "should assign a name to name" do
+	  pending
+	end
+  
+	it "should assign a date to date" do
+	  pending
+	end
+end
