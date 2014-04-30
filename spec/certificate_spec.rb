74742ce1caaf572e61c4d402f3bf6b2eee311579
Thomas Ochman <thomas@nocebo.se>
2014-04-30 18:19:56 +0200

    updated certificate_spec.rb

--- spec/certificate_spec.rb
+++ spec/certificate_spec.rb
@@ -8,12 +8,48 @@ require 'spec_helper.rb'
     end
     
     describe "validations" do
-      it 'should NOT be valid without a name' do
-        build(:certificate, :student_name => '').should_not be_valid
+      context 'valid attributes' do
+        it "should be valid with a name" do
+          create(:certificate, :student_name => "Thomas").should be_valid
+        end
+        it "should be valid with a course title" do
+          create(:certificate, :course_name => "Course Title").should be_valid
+        end
+        
+        it "should be valid with a date" do
+          create(:certificate, :generated_at => "2014-01-01").should be_valid
+        end
+        
       end
       
-      it "should be valid with a name" do
-        create(:certificate, :student_name => "Thomas").should be_valid
+      context 'invalid attributes' do
+        before do
+          @certificate = FactoryGirl.create(:certificate)
+        end
+           
+        it 'should NOT be valid without a name' do
+          build(:certificate, :student_name => '').should_not be_valid
+          @certificate.student_name = ''
+          expect(@certificate.save).to be_false
+        end
+        
+        it 'should NOT be valid without a course title' do
+          build(:certificate, :course_name => '').should_not be_valid
+          @certificate.course_name = ''
+          expect(@certificate.save).to be_false
+          
+        end
+        
+        it 'should NOT be valid without a date' do
+          build(:certificate, :generated_at => '').should_not be_valid
+          @certificate.generated_at = ''
+          expect(@certificate.save).to be_false
+          
+        end
+        
+        it 'should NOT be valid with a badly formatted date' do
+          build(:certificate, :generated_at => '140101').should_not be_valid
+        end
       end
     end
   end
