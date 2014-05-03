8e09463c55afd099cd91d7a0a8191b81cbf800ca
Thomas Ochman <thomas@nocebo.se>
2014-05-03 20:18:51 +0200

    fix for date validator

--- date_validator.rb
+++ date_validator.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class DateValidator < ActiveModel::Validator
   def validate(record)
-    Date.parse(record.generated_at)
+    Date.parse(record.generated_at.to_s)
   rescue ArgumentError
     record.errors.add(:generated_at, "invalid date format")
   end
